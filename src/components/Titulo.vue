8629ddf9ff684cc7a0b060b9721e84bc560b4315
MaquinoBSC <martinare.ipn@gmail.com>
2021-12-02 19:10:39 -0600

    v-model y comunicacion entre componentes con composition API

---
+++ src/components/Titulo.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Titulo dinamico: {{signoPeso}}</h1>
+</template>
+
+<script>
+import { computed } from '@vue/reactivity';
+
+export default {
+    props: {
+        contador: Number
+    },
+    setup(props){
+        console.log(props.contador.contador);
+        const signoPeso= computed(()=> {
+            return '$' + props.contador;
+        });
+
+
+        return {
+            signoPeso,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="about">
+    <Titulo :contador="contador" />
     <h1 :style="{'color': setColor}">Contador: {{ contador }} </h1>
     <button @click="disminuir()">Disminuir</button>
     <button @click="aumentar()">Aumentar</button>
+    <hr>
+
+    <input type="text" v-model="texto">
+    <p>{{ texto }}</p>
   </div>
 </template>
 
 <script>
 import { computed, ref } from '@vue/reactivity';
+import Titulo from '../components/Titulo.vue';
 
 export default {
+  components: {
+    Titulo
+  },
   setup(){
     const contador= ref(0);
+    const texto= ref('');
 
     const aumentar= ()=> {
       contador.value++;
@@ -31,6 +41,7 @@ export default {
       disminuir,
       aumentar,
       setColor,
+      texto,
     }
   }
 }
