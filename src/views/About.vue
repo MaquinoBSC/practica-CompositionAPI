f6e5962d6fce2c320865f6507e7a7c7039edac1e
MaquinoBSC <martinare.ipn@gmail.com>
2021-12-08 20:04:30 -0600

    Emit, con composition API

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,8 +2,10 @@
   <div class="about">
     <Titulo :contador="contador" />
     <h1 :style="{'color': setColor}">Contador: {{ contador }} </h1>
-    <button @click="disminuir()">Disminuir</button>
-    <button @click="aumentar()">Aumentar</button>
+    <!-- <button @click="disminuir()">Disminuir</button>
+    <button @click="aumentar()">Aumentar</button> -->
+    <Btn :textoBoton="'Aumentar'" @accion="aumentar"/>
+    <Btn :textoBoton="'Disminuir'" @accion="disminuir"/>
     <hr>
 
     <input type="text" v-model="texto">
@@ -14,10 +16,12 @@
 <script>
 import { computed, ref } from '@vue/reactivity';
 import Titulo from '../components/Titulo.vue';
+import Btn from '../components/Btn.vue';
 
 export default {
   components: {
-    Titulo
+    Titulo,
+    Btn,
   },
   setup(){
     const contador= ref(0);
