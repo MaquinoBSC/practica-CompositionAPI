86516c79f7784aec7f71117e242a04a7dd77f687
MaquinoBSC <martinare.ipn@gmail.com>
2021-12-08 20:38:23 -0600

    Reutilizando logica con hooks o composables

---
+++ src/views/Contador.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Contador: {{ contador }}</h1>
+  <Btn :textoBoton="'Aumnetar'" @accion="aumentar" />
+  <Btn :textoBoton="'Disminuir'" @accion="disminuir" />
+</template>
+
+<script>
+import { useContador } from '../hooks/useContador';
+import Btn from '../components/Btn.vue';
+
+
+export default {
+    components: {
+        Btn,
+    },
+    setup(){
+        return { ...useContador() }
+    }
+}
+</script>
+
+<style>
+
+</style>
